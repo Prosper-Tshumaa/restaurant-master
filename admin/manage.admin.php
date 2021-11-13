67bc2f5fb226665c2288529fc48271816d871784
Prosper-Tshumaa <tshumaprosper11@gmail.com>
2021-11-13 16:09:03 +0200

    Changes

--- admin/manage.admin.php
+++ admin/manage.admin.php
@@ -5,6 +5,12 @@
               <div class="wrapper">
                 <h1>Manage admin</h1>
 
+                <br /><br />
+
+                <!-- Button to add Admin -->
+                <a href="add-admin.php" class="btn-primary">Add Admin</a>
+
+                <br /><br /><br />
                <table class="tbl-full">
                  <tr>
                    <th>S.N.</th>
@@ -17,8 +23,26 @@
                        <td>Prosper Tshuma</td>
                        <td>prospertshuma</td>
                        <td>
-                         Update Admin
-                         Delete Admin
+                      <a href="#" class="btn-secondary">Update Admin</a>
+                      <a href="#" class="btn-danger">Delete Admin</a>
+                       </td>
+                  </tr>
+                  <tr>
+                       <td>2.</td>
+                       <td>Prosper Tshuma</td>
+                       <td>prospertshuma</td>
+                       <td>
+                       <a href="#" class="btn-secondary">Update Admin</a>
+                      <a href="#" class="btn-danger">Delete Admin</a>
+                       </td>
+                  </tr>
+                  <tr>
+                       <td>3.</td>
+                       <td>Prosper Tshuma</td>
+                       <td>prospertshuma</td>
+                       <td>
+                       <a href="#" class="btn-secondary">Update Admin</a>
+                      <a href="#" class="btn-danger">Delete Admin</a>
                        </td>
                   </tr>
                </table> 
