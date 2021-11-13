6f433b212718a15d85229b0ef325005a137e474e
Prosper-Tshumaa <tshumaprosper11@gmail.com>
2021-11-13 15:00:01 +0200

    Changes

--- admin/manage.admin.php
+++ admin/manage.admin.php
@@ -5,7 +5,14 @@
               <div class="wrapper">
                 <h1>Manage admin</h1>
 
-
+               <table>
+                 <tr>
+                   <th>S.N.</th>
+                   <th>Full Name</th>
+                   <th>Username</th>
+                   <th>Actions</th>                 
+                  </tr>
+               </table> 
 
               </div>
         </div>
